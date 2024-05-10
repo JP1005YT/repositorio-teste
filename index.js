ba4ad9b13cb50474d81aeb371ade1ac9351a3756
JP1005YT <godlolpro32@gmail.com>
2024-05-10 15:38:18 -0300

    "fix"

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
-import express from 'express'
+import express from 'express';
+import http from 'http';
 
-const app = express()
-app.use(express.static("public"))
+const app = express();
 
-app.listen(5500,()=>{
-    console.log('servidor aberto')
-})
+// Middleware para servir arquivos estáticos
+app.use(express.static("public"));
+
+// Criação do servidor HTTP
+const server = http.createServer(app);
+
+// Iniciando o servidor na porta 5500
+server.listen(5500, () => {
+    console.log('Servidor HTTP aberto na porta 5500');
+});
